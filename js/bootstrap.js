74055af01b7a3b2dda17757e7dbe201ef2e100d8
YosseLine <yoseline_96@hotmail.com>
2018-02-11 03:11:52 -0500

    Mejorando vista responsive

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
   $('.dropdown-menu').dropdown();
+  $('#myTabs a').click(function (event) {
+  event.preventDefault()
+  $(this).tab('show');
+});
+$('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+$('#myTabs a:first').tab('show') // Select first tab
+$('#myTabs a:last').tab('show') // Select last tab
+$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
 });
